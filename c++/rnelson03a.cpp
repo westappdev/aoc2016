ae71c5237daa8bf13ee9838b33135f8536782826
Ross Nelson <ross.nelson@gmail.com>
2016-12-03 09:00:10 -0600

    Add FreeBSD paths to compilation command for Boost

--- c++/rnelson03a.cpp
+++ c++/rnelson03a.cpp
@@ -2,7 +2,7 @@
  * Advent of Code 2016
  * Day 3 (part 1)
  *
- * Command: clang++ --std=c++14 rnelson03a.cpp
+ * Command: clang++ --std=c++14 -I/usr/local/include -L/usr/local/lib rnelson03a.cpp
  *
  */
 
--- c++/rnelson03b.cpp
+++ c++/rnelson03b.cpp
@@ -2,7 +2,7 @@
  * Advent of Code 2016
  * Day 3 (part 2)
  *
- * Command: clang++ --std=c++14 rnelson03b.cpp
+ * Command: clang++ --std=c++14 -I/usr/local/include -L/usr/local/lib rnelson03b.cpp
  *
  */
 
