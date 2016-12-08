2bd25422e07a384df51c21084879b168cb5758dc
Kevin Murray <kevin.murray.38@gmail.com>
2016-12-08 16:30:03 -0600

    Day 7 & 8 Commit
    Lots of cleanup needed

---
+++ c#/kmurrayDay7.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2016 {
+   internal static class Day7 {
+      public static void Process() {
+
+         var inputFile = File.ReadAllText(Path.Combine("Inputs", "Day7a.txt")).Replace("\r", "").Split('\n');
+
+         var tlsTotal = 0;
+         var sslTotal = 0;
+
+         foreach (var input in inputFile) {
+            if (SupportTLS(input)) {
+               ++tlsTotal;
+            } 
+            if (SupportSSL(input)) {
+               ++sslTotal;
+            }
+         }
+
+         Console.WriteLine("Total TLS = " + tlsTotal);
+         Console.WriteLine("Total SSL = " + sslTotal);
+
+      }
+
+      private static bool SupportSSL(string input) {
+         var aba = new HashSet<string>();
+         var bab = new HashSet<string>();
+
+         var isHypernet = false;
+
+         for (var index = 0; index < input.Length - 2; ++index) {
+            isHypernet = (input[index].Equals('[')) || (!input[index].Equals(']')) && isHypernet;
+
+            if ((!input[index].Equals(input[index + 2]) ||
+                 (input[index].Equals(input[index + 1]) || (input[index + 1].Equals('[') || input[index + 1].Equals(']')))))
+               continue;
+
+            if (isHypernet) {
+               bab.Add((input[index].ToString() + input[index + 1].ToString() + input[index + 2].ToString()));
+
+            }
+            else {
+               aba.Add((input[index].ToString() + input[index + 1].ToString() + input[index + 2].ToString()));
+            }
+         }
+
+         return aba.Any(key => bab.Contains(key[1].ToString() + key[0].ToString() + key[1].ToString()));
+      }
+
+      private static bool SupportTLS(string input) {
+         var isAbba = false;
+         var isHypernet = false;
+         
+         for (var index = 0; index < input.Length - 3; ++index) {
+            isHypernet = (input[index].Equals('[')) || (!input[index].Equals(']')) && isHypernet;
+
+            if ((!input[index].Equals(input[index + 3]) || !input[index + 1].Equals(input[index + 2])) ||
+                (input[index].Equals(input[index + 1]) || input[index + 2].Equals(input[index + 3])))
+               continue;
+
+            if (isHypernet) {
+               break;
+            }
+            else {
+               isAbba = true;
+            }
+         }
+
+         return (isAbba && !isHypernet);
+      }
+   }
+}
+
+/*
+--- Day 7: Internet Protocol Version 7 ---
+
+While snooping around the local network of EBHQ, you compile a list of IP addresses(they're IPv7, of course; IPv6 is much too limited). You'd like to figure out which IPs support TLS (transport-layer snooping).
+
+An IP supports TLS if it has an Autonomous Bridge Bypass Annotation, or ABBA.An ABBA is any four-character sequence which consists of a pair of two different characters followed by the reverse of that pair, such as xyyx or abba.However, the IP also must not have an ABBA within any hypernet sequences, which are contained by square brackets.
+
+For example:
+
+abba[mnop] qrst supports TLS (abba outside square brackets).
+abcd[bddb] xyyx does not support TLS(bddb is within square brackets, even though xyyx is outside square brackets).
+aaaa[qwer] tyui does not support TLS(aaaa is invalid; the interior characters must be different).
+ioxxoj[asdfgh] zxcvbn supports TLS(oxxo is outside square brackets, even though it's within a larger string).
+How many IPs in your puzzle input support TLS?
+
+ Your puzzle answer was 105.
+
+--- Part Two ---
+
+
+ You would also like to know which IPs support SSL (super-secret listening).
+
+An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, anywhere in the supernet sequences(outside any square bracketed sections), and a corresponding Byte Allocation Block, or BAB, anywhere in the hypernet sequences.An ABA is any three-character sequence which consists of the same character twice with a different character between them, such as xyx or aba.A corresponding BAB is the same characters but in reversed positions: yxy and bab, respectively.
+
+For example:
+
+aba[bab] xyz supports SSL(aba outside square brackets with corresponding bab within square brackets).
+xyx[xyx] xyx does not support SSL(xyx, but no corresponding yxy).
+aaa[kek] eke supports SSL(eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different).
+zazbz[bzb] cdb supports SSL(zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap).
+How many IPs in your puzzle input support SSL?
+
+Your puzzle answer was 258.
+*/
---
+++ c#/kmurrayDay8.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode2016 {
+   internal static class Day8 {
+      private const int Row = 6;
+      private const int Column = 50;
+
+      private static void InitializeScreen(IDictionary<string, string> screen) {
+         for (var row = 0; row < Row; ++row) {
+            for (var col = 0; col < Column; ++col) {
+               screen.Add((row + "," + col), " ");
+            }
+         }
+      }
+
+      internal static void Process() {
+         var screen = new Dictionary<string, string>();
+
+         InitializeScreen(screen);
+
+         var inputFile = File.ReadAllText(Path.Combine("Inputs", "Day8a.txt")).Replace("\r", "").Split('\n');
+
+         foreach (var input in inputFile) {
+            var command = input.Split();
+
+            if (command[0].Equals("rect")) {
+               SetRectangle(command[1].Split('x'), screen);
+            }
+
+            else if (command[0].Equals("rotate")) {
+               var position = command[2].Split('=');
+               Rotate(command[1], Convert.ToInt32(position[1]), Convert.ToInt32(command[4]), screen);
+            }
+         }
+
+         Console.WriteLine("Done Total lit pixels = " +  screen.Count(c => c.Value == "#"));
+         Console.WriteLine("Password = ");
+         for (var row = 0; row < Row; ++row) {
+            for (var col = 0; col < Column; ++col) {
+               Console.Write(screen[row + "," + col]);
+            }
+            Console.WriteLine();
+         }
+      }
+
+      private static void Rotate(string axis, int position, int shift, IDictionary<string, string> screen) {
+
+         if (axis.Equals("column")) {
+            for (var count = 0; count < shift; ++count) {
+               var cur = screen[(Row - 1) + "," + position];
+
+               for (var row = 0; row < Row; ++row) {
+                  var temp = screen[row + "," + position];
+                  screen[row + "," + position] = cur;
+                  cur = temp;
+               }
+            }
+         }
+         else if (axis.Equals("row"))
+         {
+            for (var count = 0; count < shift; ++count) {
+               var cur = screen[position + "," + (Column - 1).ToString()];
+
+               for (var col = 0; col < Column; ++col) {
+                  var temp = screen[position + "," + col];
+                  screen[position + "," + col] = cur;
+                  cur = temp;
+               }
+            }
+         }
+      }
+
+      private static void SetRectangle(IReadOnlyList<string> dim, IDictionary<string, string> screen) {
+         for (var row = 0; row < Convert.ToInt32(dim[1]); ++row) {
+            for (var col = 0; col < Convert.ToInt32(dim[0]); ++col) {
+               screen[row + "," + col] = "#";
+            }
+         }
+      }
+   }
+}
+
+/*
+--- Day 8: Two-Factor Authentication ---
+
+You come across a door implementing what you can only assume is an implementation of two-factor authentication after a long game of requirements telephone.
+
+To get past the door, you first swipe a keycard (no problem; there was one on a nearby desk). Then, it displays a code on a little screen, and you type that code on a keypad. Then, presumably, the door unlocks.
+
+Unfortunately, the screen has been smashed. After a few minutes, you've taken everything apart and figured out how it works. Now you just have to work out what the screen would have displayed.
+
+The magnetic strip on the card you swiped encodes a series of instructions for the screen; these instructions are your puzzle input. The screen is 50 pixels wide and 6 pixels tall, all of which start off, and is capable of three somewhat peculiar operations:
+
+rect AxB turns on all of the pixels in a rectangle at the top-left of the screen which is A wide and B tall.
+rotate row y=A by B shifts all of the pixels in row A (0 is the top row) right by B pixels. Pixels that would fall off the right end appear at the left end of the row.
+rotate column x=A by B shifts all of the pixels in column A (0 is the left column) down by B pixels. Pixels that would fall off the bottom appear at the top of the column.
+For example, here is a simple sequence on a smaller screen:
+
+rect 3x2 creates a small rectangle in the top-left corner:
+
+###....
+###....
+.......
+rotate column x=1 by 1 rotates the second column down by one pixel:
+
+#.#....
+###....
+.#.....
+rotate row y=0 by 4 rotates the top row right by four pixels:
+
+....#.#
+###....
+.#.....
+rotate column x=1 by 1 again rotates the second column down by one pixel, causing the bottom pixel to wrap back to the top:
+
+.#..#.#
+#.#....
+.#.....
+As you can see, this display technology is extremely powerful, and will soon dominate the tiny-code-displaying-screen market. That's what the advertisement on the back of the display tries to convince you, anyway.
+
+There seems to be an intermediate check of the voltage used by the display: after you swipe your card, if the screen did work, how many pixels should be lit?
+
+Your puzzle answer was 115.
+
+--- Part Two ---
+
+You notice that the screen is only capable of displaying capital letters; in the font it uses, each letter is 5 pixels wide and 6 tall.
+
+After you swipe your card, what code is the screen trying to display?
+
+Your puzzle answer was EFEYKFRFIJ.
+*/
